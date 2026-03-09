fdebfcdc9d53ebe0fa2a11641f5aef3129c385a9
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-03-08 23:13:31 -0300

    🗃️ use composite primary keys for UserProfileAddictionEntity

--- app/src/main/java/com/wesley/beefree/storage/adapters/db/entities/UserProfileAddictionEntity.kt
+++ app/src/main/java/com/wesley/beefree/storage/adapters/db/entities/UserProfileAddictionEntity.kt
@@ -2,11 +2,10 @@ package com.wesley.beefree.storage.adapters.db.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "UserProfileAddiction",
-    primaryKeys = ["user_profile_id", "addiction_type_id"]
+    primaryKeys = ["user_profile_id", "addiction_type_id"],
 )
 data class UserProfileAddictionEntity(
     @ColumnInfo(name = "user_profile_id") val userProfileId: Int,
