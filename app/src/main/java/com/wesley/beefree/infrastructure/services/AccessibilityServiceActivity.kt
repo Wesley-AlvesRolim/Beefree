78156327222d3e22f8e2f6400215eeb71163fb55
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-03-13 19:10:11 -0300

    ✨ `RelapseRecorderModule` for saving in the `RelapseHistory`

--- app/src/main/java/com/wesley/beefree/infrastructure/services/AccessibilityServiceActivity.kt
+++ app/src/main/java/com/wesley/beefree/infrastructure/services/AccessibilityServiceActivity.kt
@@ -9,24 +9,45 @@ import com.wesley.beefree.domain.detection.KeywordsDetectionEngine
 import com.wesley.beefree.domain.entities.AddictionTypeEnum
 import com.wesley.beefree.infrastructure.bus.adapters.InMemoryEventBus
 import com.wesley.beefree.infrastructure.dispatcher.AccessibilityEventDispatcher
+import com.wesley.beefree.infrastructure.history.RelapseRecorderModule
 import com.wesley.beefree.infrastructure.intervention.OverlayInterventionModule
+import com.wesley.beefree.storage.adapters.RoomAddictionRepository
 import com.wesley.beefree.storage.adapters.SharedPreferencesKeyValueStorage
+import com.wesley.beefree.storage.adapters.db.AppDatabase
+import com.wesley.beefree.storage.ports.AddictionRepository
 import com.wesley.beefree.storage.repositories.KeyValueStorageRepository
 import com.wesley.beefree.ui.adapters.AndroidOverlayInterventionUI
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 
-class AccessibilityServiceActivity : AccessibilityService() {
+class AccessibilityServiceActivity :
+    AccessibilityService(),
+    CoroutineScope {
     private val tag = "AccessibilityServiceActivity"
-    private var repository: KeyValueStorageRepository? = null
+    private var keyValueStorageRepository: KeyValueStorageRepository? = null
+    private var addictionRepository: AddictionRepository? = null
+    private val job = SupervisorJob()
+    override val coroutineContext = Dispatchers.Main + job
 
     private val eventBus = InMemoryEventBus()
     private lateinit var dispatcher: AccessibilityEventDispatcher
     private lateinit var keyWordsDetectionEngine: KeywordsDetectionEngine
     private lateinit var interventionModule: OverlayInterventionModule
+    private lateinit var relapseRecorderModule: RelapseRecorderModule
 
     override fun onCreate() {
         super.onCreate()
-        repository = KeyValueStorageRepository(SharedPreferencesKeyValueStorage(this))
-        dispatcher = AccessibilityEventDispatcher(eventBus, repository)
+        keyValueStorageRepository =
+            KeyValueStorageRepository(SharedPreferencesKeyValueStorage(this))
+        val db = AppDatabase.getDatabase(this)
+        addictionRepository =
+            RoomAddictionRepository(
+                db.addictionTypeDao(),
+                db.addictionKeywordDao(),
+                db.relapseHistoryDao(),
+            )
+        dispatcher = AccessibilityEventDispatcher(eventBus, keyValueStorageRepository)
 
         val keywordsByAddictionType =
             mapOf(
@@ -37,6 +58,7 @@ class AccessibilityServiceActivity : AccessibilityService() {
 
         val interventionUI = AndroidOverlayInterventionUI(this)
         interventionModule = OverlayInterventionModule(eventBus, interventionUI)
+        relapseRecorderModule = RelapseRecorderModule(eventBus, addictionRepository!!, this)
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
@@ -48,5 +70,10 @@ class AccessibilityServiceActivity : AccessibilityService() {
 
     override fun onInterrupt() {
         Log.d(tag, "Stop AccessibilityService")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
