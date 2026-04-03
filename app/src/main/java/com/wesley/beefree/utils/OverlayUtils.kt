e5fd2fe8b3ad41b8178bda544fd1dc37b2cb892c
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-04-03 17:22:04 -0300

    ♻️ use time strategy to avoid many calls of `OverlayUtils`

--- app/src/main/java/com/wesley/beefree/utils/OverlayUtils.kt
+++ app/src/main/java/com/wesley/beefree/utils/OverlayUtils.kt
@@ -9,12 +9,19 @@ import com.wesley.beefree.ui.INTERVENTION_REASON
 import com.wesley.beefree.ui.InterventionActivity
 
 object OverlayUtils {
+    private var lastActivityAttempt = 0L
+    private var lastServiceAttempt = 0L
+    private const val ATTEMPT_COOLDOWN = 1000L
+
     fun startInterventionActivity(
         context: Context,
         reason: String = "",
     ) {
         if (InterventionActivity.isRunning) return
-        InterventionActivity.isRunning = true
+        val now = System.currentTimeMillis()
+        if (now - lastActivityAttempt < ATTEMPT_COOLDOWN) return
+        lastActivityAttempt = now
+
         val intent =
             Intent(context, InterventionActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK
@@ -28,7 +35,10 @@ object OverlayUtils {
         reason: String = "",
     ) {
         if (OverlayServiceActivity.isRunning) return
-        OverlayServiceActivity.isRunning = true
+        val now = System.currentTimeMillis()
+        if (now - lastServiceAttempt < ATTEMPT_COOLDOWN) return
+        lastServiceAttempt = now
+
         val intent =
             Intent(context, OverlayServiceActivity::class.java).apply {
                 putExtra(OVERLAY_TRIGGERED_BY_REASON, reason)
