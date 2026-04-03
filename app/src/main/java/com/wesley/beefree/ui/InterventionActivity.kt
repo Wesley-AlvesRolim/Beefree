bc87b41a93cb753554629ce3ce0678951f48e1d1
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-04-03 17:24:49 -0300

    💄 `InterventionActivity` respect the smartphone bars paddings

--- app/src/main/java/com/wesley/beefree/ui/InterventionActivity.kt
+++ app/src/main/java/com/wesley/beefree/ui/InterventionActivity.kt
@@ -3,6 +3,7 @@ package com.wesley.beefree.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.wesley.beefree.ui.components.OverlayUI
 
 const val INTERVENTION_REASON = "INTERVENTION_REASON"
@@ -12,20 +13,37 @@ class InterventionActivity : ComponentActivity() {
         var isRunning = false
     }
 
+    override fun onStart() {
+        super.onStart()
+        isRunning = true
+    }
+
+    override fun onStop() {
+        isRunning = false
+        super.onStop()
+    }
+
+    override fun onNewIntent(intent: android.content.Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        updateUI()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        isRunning = true
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        updateUI()
+    }
+
+    private fun updateUI() {
         val reason = intent.getStringExtra(INTERVENTION_REASON) ?: ""
         setContent {
             OverlayUI(
                 reason = reason,
-                onCloseRequest = { finish() },
+                onCloseRequest = {
+                    finish()
+                },
             )
         }
-    }
-
-    override fun onDestroy() {
-        isRunning = false
-        super.onDestroy()
     }
 }
