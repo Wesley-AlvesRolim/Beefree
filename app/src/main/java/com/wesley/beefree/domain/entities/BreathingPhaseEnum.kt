41748b6615b780d817dbf1ab9c84a3146d51f063
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-05-06 00:51:15 -0300

    ✨ `BreathingPhaseEnum`

---
+++ app/src/main/java/com/wesley/beefree/domain/entities/BreathingPhaseEnum.kt
@@ -0,0 +1,13 @@
+package com.wesley.beefree.domain.entities
+
+import androidx.annotation.StringRes
+import com.wesley.beefree.R
+
+enum class BreathingPhaseEnum(
+    val durationSeconds: Int,
+    @StringRes val labelRes: Int,
+) {
+    INHALE(4, R.string.urge_surfing_phase_inhale),
+    HOLD(7, R.string.urge_surfing_phase_hold),
+    EXHALE(8, R.string.urge_surfing_phase_exhale),
+}
