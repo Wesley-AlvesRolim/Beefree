5b2652a291dfce769292dd53eead4fcb9498d722
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-05-07 17:37:29 -0300

    ➕ add `kotlinx-coroutines-test`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
     testImplementation(libs.androidx.test.core.ktx)
     testImplementation(libs.mockito.core)
     testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.mockito.android)
