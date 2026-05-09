b57fb1f1dc00dd985e0facbf174b4970c9a18b8f
Wesley Alves Rolim <walvesrolim@gmail.com>
2026-05-08 22:37:43 -0300

    ♻️ add more actions in flows

--- app/src/main/java/com/wesley/beefree/domain/intervention/strategies/ActHelpInterventionFlow.kt
+++ app/src/main/java/com/wesley/beefree/domain/intervention/strategies/ActHelpInterventionFlow.kt
@@ -26,15 +26,33 @@ object ActHelpInterventionFlow {
             // Regulation
             ActionSuggestion("help_intervention.act_actions.breathe_2min", "regulation"),
             ActionSuggestion("help_intervention.act_actions.drink_water", "regulation"),
+            ActionSuggestion("help_intervention.act_actions.meditate", "regulation"),
+            ActionSuggestion("help_intervention.act_actions.pray", "regulation"),
+            ActionSuggestion("help_intervention.act_actions.acknowledge_urge", "regulation"),
+            ActionSuggestion("help_intervention.act_actions.stretch", "regulation"),
             ActionSuggestion("help_intervention.act_actions.leave_environment", "regulation"),
+            ActionSuggestion("help_intervention.act_actions.put_phone_away", "regulation"),
             // Context change
-            ActionSuggestion("help_intervention.act_actions.block_app", "context"),
             ActionSuggestion("help_intervention.act_actions.go_another_room", "context"),
             ActionSuggestion("help_intervention.act_actions.take_walk", "context"),
+            ActionSuggestion("help_intervention.act_actions.read_5min", "context"),
+            ActionSuggestion("help_intervention.act_actions.study_10min", "context"),
+            ActionSuggestion("help_intervention.act_actions.journal", "context"),
+            ActionSuggestion("help_intervention.act_actions.plan_tasks", "context"),
+            ActionSuggestion("help_intervention.act_actions.work_10min", "context"),
+            ActionSuggestion("help_intervention.act_actions.do_productive", "context"),
             // Value connection
+            ActionSuggestion("help_intervention.act_actions.call_family", "values"),
+            ActionSuggestion("help_intervention.act_actions.help_at_home", "values"),
+            ActionSuggestion("help_intervention.act_actions.read_scripture", "values"),
+            ActionSuggestion("help_intervention.act_actions.practice_gratitude", "values"),
             ActionSuggestion("help_intervention.act_actions.message_someone", "values"),
-            ActionSuggestion("help_intervention.act_actions.work_10min", "values"),
-            ActionSuggestion("help_intervention.act_actions.do_productive", "values"),
+            ActionSuggestion("help_intervention.act_actions.listen_someone", "values"),
+            ActionSuggestion("help_intervention.act_actions.help_someone", "values"),
+            ActionSuggestion("help_intervention.act_actions.send_kind_message", "values"),
+            ActionSuggestion("help_intervention.act_actions.show_affection", "values"),
+            ActionSuggestion("help_intervention.act_actions.message_loved_one", "values"),
+            ActionSuggestion("help_intervention.act_actions.write_feelings", "values"),
         )
 
     val steps: List<HelpInterventionStep> =
